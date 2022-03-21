cdd03afc537c6837b1086c3d2a130a9fc5089e00
kachireddy2003 <100481988+kachireddy2003@users.noreply.github.com>
2022-03-21 10:09:43 +0530

    Add files via upload

---
+++ POWERmethod(base and exponataial calculation).c
@@ -0,0 +1,12 @@
+// POWER CALCULATION EXPONATIAL CALUCULATION
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int base,power,x;
+    base = 5;
+    power= 3;
+    x=pow(base,power);
+    printf("%d",x);
+}
+//power is exponatial 
